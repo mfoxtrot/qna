7b00f90e0213b7b20ce50bbf4bae1b0b8256acdd
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-03-24 16:10:01 +0300

    Tests for Answer&Question models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   Shoulda::Matchers.configure do |config|
-    configure.integrate do |with|
+    config.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
