4ff30df8d4df08dfb149118aaabf90c11a406ccc
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-03-27 00:59:14 +0300

    Spec for Questions#create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -38,4 +38,16 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question' do
+        expect { post :create, params: { question: attributes_for(:question)} }.to change(Question, :count).by(1)
+      end
+      it 'redirects to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+  end
 end
