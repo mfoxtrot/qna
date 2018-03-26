5931af002b8db3601338ecc8838fda009e3ab361
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-03-27 00:29:27 +0300

    Spec for Questions#show method

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+    before do
+      get :show
+    end
+
+    it 'assigns object to @question variable' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
