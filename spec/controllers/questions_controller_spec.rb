9ea264dd400a2dc7078633cbf57297af6bd1c77c
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-03-27 00:09:58 +0300

    Spec for Questions#index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+    before do
+      get :index
+    end
 
+    it 'pupulates an array of all questions' do
+      expect(assigns(:questions)).to_match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
 end
