cac8ac904fcc8af564be06f038a99b3df5c084d3
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-03-27 23:34:40 +0300

    Spec for Answers#new

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe 'GET #new' do
+    let(:question) { create(:question) }
 
+    before { get :new, params: { question_id: question.id } }
+
+    it 'assigns a new Question to @question variable' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'should be a nested resource to question' do
+      expect(assigns(:answer).question).to eq question
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
