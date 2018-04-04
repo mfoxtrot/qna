f25ec657336e874fd519e82a4051260409191f84
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-04-04 23:55:42 +0300

    Spec 4: User's able to create a question

---
+++ spec/features/user_creates_question_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User creates a question', %q{
+  Any User is able to create a question
+} do
+  scenario 'User try to create a question' do
+
+    visit new_question_path
+    fill_in 'Title', with: 'Title of the new question'
+    fill_in 'Body', with: 'Lorem ipsum dolor sit amet'
+    click_on 'Create a question'
+
+    expect(page).to have_content 'The question created successfully'
+
+  end
+end
