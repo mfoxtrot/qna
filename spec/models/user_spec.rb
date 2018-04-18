637c4f276526d82842dfe292549bca731efcf2ec
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-04-18 22:08:27 +0300

    Fix 3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,30 +11,12 @@ RSpec.describe User do
     it { should validate_presence_of :password }
   end
 
-  describe 'author_of? method' do
+  describe '#author_of?' do
     let!(:user) { create(:user) }
+    let(:users_question) { create(:question, author_id: user.id) }
 
-    describe 'for questions' do
-      let(:users_question) { create(:question, author: user) }
-      let(:question) { create(:question) }
-      it 'should be the author_of? for created questions' do
-        expect(user.author_of?(users_question)).to be true
-      end
-      it 'should not be the author_of? for created questions' do
-        expect(user.author_of?(question)).not_to be true
-      end
-    end
-
-    describe 'for answers' do
-      let!(:users_answer) { create(:answer, author: user) }
-      let!(:answer) { create(:answer) }
-
-      it 'should be the author_of? for created answers' do
-        expect(user.author_of?(users_answer)).to be true
-      end
-      it 'should not be the author_of? for created answers' do
-        expect(user.author_of?(answer)).not_to be true
-      end
+    it '#author_of?' do
+      expect(user).to be_author_of(users_question)
     end
   end
 end
