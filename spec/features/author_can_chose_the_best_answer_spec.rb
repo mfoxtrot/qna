f2b913176c1d4faa63e40a01176522672c12ebb6
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-05-06 17:55:15 +0300

    US:Author can choose the best answer. Initial spec

---
+++ spec/features/author_can_chose_the_best_answer_spec.rb
@@ -0,0 +1,29 @@
+require 'features_helper.rb'
+
+feature 'Author can choose the best answer' do
+
+  given!(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+  given!(:user) { create(:user) }
+
+  describe 'Non authenticated user' do
+    scenario 'cant choose the best answer for the question' do
+      visit question_path(question)
+      expect(page).not_to have_link 'Mark as the best'
+    end
+  end
+
+  describe 'Authenticated user' do
+    describe 'if he is not the author of the question' do
+      scenario 'can not choose the best answer' do
+        sign_in_user(user)
+        visit question_path(question)
+        expect(page).not_to have_link 'Mark as the best'
+      end
+    end
+
+    describe 'if he is the author of the question' do
+      scenario 'can choose the best answer'
+    end
+  end
+end
