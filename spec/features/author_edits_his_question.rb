948c4fb0985ba33888c4b6bbe57cbb7a5a01c6ac
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-05-06 14:28:44 +0300

    US:author can edit his question. Author can see Edit link

--- spec/features/author_edits_his_question.rb
+++ spec/features/author_edits_his_question.rb
@@ -5,26 +5,32 @@ feature 'Author edits his question', %q{
   to edit his questions
 } do
 
-  given!(:user) { create(:user) }
-  given!(:question) { create(:question) }
-
   describe 'Authenticated user' do
+    given!(:user) { create(:user) }
     before { sign_in_user(user) }
 
     describe 'if he is an author of the question' do
-      scenario 'view Edit link on questions page'
+      given!(:users_question) { create(:question, author: user)}
+      before { visit questions_path }
+
+      scenario 'view Edit link on questions page' do
+        expect(page).to have_link 'Edit'
+      end
       scenario 'edit body of his question'
     end
 
     describe 'if he is not an author of the question' do
+      given!(:question) { create(:question) }
+      before { visit questions_path }
+
       scenario 'does not see Edit link on questions page' do
-        visit questions_path
         expect(page).not_to have_link 'Edit'
       end
     end
   end
 
   describe 'Non-authenticated user' do
+    given!(:question) { create(:question) }
     scenario 'does not see Edit link on questions page' do
       visit questions_path
       expect(page).not_to have_link 'Edit'
