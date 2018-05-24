d7a5fa172c35b56e4d7633d96b684b933de77dfb
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-05-24 23:37:45 +0300

    US: User can attach multiple files to questions and answers

--- spec/features/user_can_add_files_to_answers_spec.rb
+++ spec/features/user_can_add_files_to_answers_spec.rb
@@ -19,4 +19,18 @@ feature 'User can add files to answers' do
       expect(page).to have_link 'file_upload_test.txt', href: '/uploads/attachment/file/1/file_upload_test.txt'
     end
   end
+
+  scenario 'User adds multiple files while answering the question', js: true do
+    fill_in 'answer[body]', with: answer
+    attach_file 'File', "#{Rails.root}/spec/fixtures/files/file_upload_test.txt"
+    click_on 'add file'
+    page.all(:css, 'input[type="file"]')[1].set("#{Rails.root}/spec/fixtures/files/file_upload_test2.txt")
+
+    click_on 'Post an answer'
+
+    within '.answers' do
+      expect(page).to have_link 'file_upload_test.txt', href: '/uploads/attachment/file/1/file_upload_test.txt'
+      expect(page).to have_link 'file_upload_test2.txt', href: '/uploads/attachment/file/2/file_upload_test2.txt'
+    end
+  end
 end
--- spec/features/user_can_add_files_to_questions_spec.rb
+++ spec/features/user_can_add_files_to_questions_spec.rb
@@ -12,7 +12,7 @@ feature 'User can add files to questions', %w{
     visit questions_path
   end
 
-  scenario 'User adds a file while asking a question' do
+  scenario 'User adds a file while asking a question', js: true do
     title = Faker::Lorem.unique.sentence
     body = Faker::Lorem.unique.sentence
 
@@ -24,4 +24,22 @@ feature 'User can add files to questions', %w{
 
     expect(page).to have_link 'file_upload_test.txt', href: '/uploads/attachment/file/1/file_upload_test.txt'
   end
+
+  scenario 'User adds multiple files while asking a question', js: true do
+    title = Faker::Lorem.unique.sentence
+    body = Faker::Lorem.unique.sentence
+
+    click_on 'Ask a question'
+    fill_in 'Title', with: title
+    fill_in 'Body', with: body
+    attach_file 'File', "#{Rails.root}/spec/fixtures/files/file_upload_test.txt"
+    click_on 'add file'
+    page.all(:css, 'input[type="file"]')[1].set("#{Rails.root}/spec/fixtures/files/file_upload_test2.txt")
+
+    click_on 'Create'
+
+    expect(page).to have_link 'file_upload_test.txt', href: '/uploads/attachment/file/1/file_upload_test.txt'
+    expect(page).to have_link 'file_upload_test2.txt', href: '/uploads/attachment/file/2/file_upload_test2.txt'
+  end
+
 end
