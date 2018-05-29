4cd3b1a410dc6c7f26ad512e76fa32382891c756
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-05-29 10:05:30 +0300

    Attached files. Refactoring

--- spec/features/questions_author_can_delete_attached_files_spec.rb
+++ spec/features/questions_author_can_delete_attached_files_spec.rb
@@ -4,6 +4,7 @@ feature 'Question''s author can delete attached files' do
 
   given!(:question) { create(:question) }
   given!(:user) { create(:user) }
+  given!(:attachment) { create(:attachment, attachable: question)}
 
   context 'Non-authenticated user' do
     scenario 'Is not able to delete attached files' do
@@ -24,25 +25,13 @@ feature 'Question''s author can delete attached files' do
     end
 
     scenario 'Is able to delete his question''s files', js: true do
-      sign_in_user(user)
+      sign_in_user(question.author)
 
-      title = Faker::Lorem.unique.sentence
-      body = Faker::Lorem.unique.sentence
-
-      click_on 'Ask a question'
-      fill_in 'Title', with: title
-      fill_in 'Body', with: body
-      attach_file 'File', "#{Rails.root}/spec/fixtures/files/file_upload_test.txt"
-      click_on 'Create'
-
-      wait_for_ajax
-
-      created_question = Question.last
-      visit question_path(created_question)
+      visit question_path(question)
       within('.attachments') do
         click_on 'Delete file'
+        expect(page).not_to have_link 'Delete file'
       end
-      expect(page).not_to have_link 'file_upload_test.txt'
     end
   end
 end
