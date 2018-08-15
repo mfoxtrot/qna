d4d4ff42fc3064edcf340aa7a159934b1e163c69
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-08-15 13:11:58 +0300

    Update doorkeeper via CVE-2018-1000211

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180807155811) do
+ActiveRecord::Schema.define(version: 20180815100545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20180807155811) do
     t.string "scopes", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confidential", default: true, null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
