5ca7f6159ab7a902f7ef3a0ff75826674dd049a6
mfoxtrot <dmitrey.sokolov@gmail.com>
2018-08-30 02:32:46 +0300

    Settings for caching questions page

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Qna
     end
 
     config.active_job.queue_adapter = :sidekiq
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -29,7 +30,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
